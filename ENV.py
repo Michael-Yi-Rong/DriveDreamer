e31870225db21d2b368debd11a29da3f77f98bab
Michael <rongyi8848@foxmail.com>
2025-01-09 10:29:26 +0800

    config update

--- ENV.py
+++ ENV.py
@@ -9,7 +9,7 @@ def init_paths(base_dir=None, project_name=None):
     python_paths = [
         os.path.join(cur_dir, 'dreamer-datasets'),
         os.path.join(cur_dir, 'dreamer-models'),
-        os.path.join(cur_dir, 'dreamer-train'),
+        os.path.join(cur_dir, 'dreamer-train')
     ]
     if project_name is not None:
         python_paths.append(os.path.join(cur_dir, 'dreamer-train', 'projects', project_name))
@@ -21,14 +21,14 @@ def init_paths(base_dir=None, project_name=None):
         else:
             os.environ['PYTHONPATH'] = python_path
             
-    os.environ['TORCH_HOME'] = '/mnt/pfs/models/torch/'
-    os.environ['TRANSFORMERS_CACHE'] = '/mnt/pfs/models/huggingface/'
-    os.environ['HUGGINGFACE_HUB_CACHE'] = '/mnt/pfs/models/huggingface/'
-    os.environ['XDG_CACHE_HOME'] = '/mnt/pfs/models/xdg/'
+    os.environ['TORCH_HOME'] = '/SSD_DISK/users/rongyi/.cache/torch/'
+    os.environ['TRANSFORMERS_CACHE'] = '/SSD_DISK/users/rongyi/.cache/huggingface/hub/'
+    os.environ['HUGGINGFACE_HUB_CACHE'] = '/SSD_DISK/users/rongyi/.cache/huggingface/hub/'
+    os.environ['XDG_CACHE_HOME'] = '/SSD_DISK/users/rongyi/.cache/xdg/'
         
     # For users with huggingface network issues, uncomment the following lines for speedding up downloading
-    # os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+    os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
     
     # uncomment the following lines if you have already downloaded huggingface models, and you want set up offline mode (use the local models)
-    # os.environ['HF_HUB_OFFLINE'] = '1'
-    # os.environ['TRANSFORMERS_OFFLINE'] = '1'
+    os.environ['HF_HUB_OFFLINE'] = '1'
+    os.environ['TRANSFORMERS_OFFLINE'] = '1'
